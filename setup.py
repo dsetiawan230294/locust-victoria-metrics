522390c1acaa524ca880248deb52a3bbffb5abe2
Didit Setiawan <didit@pintu.co.id>
2025-06-17 15:12:37 +0700

    Require locust 2.29.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name="locust-victoria-metrics",
-    version="0.0.1",
+    version="0.0.2",
     description="A Locust plugin to extract test results and push metrics to Victoria Metrics",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -15,7 +15,7 @@ setup(
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     install_requires=[
-        "locust>=2.29.1",
+        "locust==2.29.1",
     ],
     classifiers=[
         "Programming Language :: Python :: 3.10",
